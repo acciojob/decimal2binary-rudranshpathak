ff756189c10be46ad62f79e80dd09caa5b83aed5
Rudransh Pathak <127862704+rudranshpathak@users.noreply.github.com>
2023-06-22 21:03:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function threeSum(arr, target) {
-// write your code here
+function decimalToBinary(decimal) {
   
+  // Handle special case for 0
+  if (decimal === 0) {
+    return "0";
+  }
+
+  let binary = "";
+
+  // Convert the decimal number to binary
+  while (decimal > 0) {
+    // Append the least significant bit (remainder) to the binary string
+    binary = (decimal % 2) + binary;
+
+    // Update the decimal number by dividing it by 2 (integer division)
+    decimal = Math.floor(decimal / 2);
+  }
+
+  return binary;
 }
 
 module.exports = threeSum;
